6eee33d53acecf4bd9511a85a6e5af5a7da05244
Euan Kemp <euank@euank.com>
2022-11-30 23:44:08 -0800

    day01, part2

--- day01/default.nix
+++ day01/default.nix
@@ -12,7 +12,18 @@ with lib; let
       sum = xs: foldl' add 0 xs;
     in
     foldl' max 0 (map sum parsed);
+
+  # part 2
+  part2Answer = input:
+    let
+      parsed = parseInput input;
+      sum = xs: foldl' add 0 xs;
+      summed = map sum parsed;
+      sorted = lists.sort (a: b: a > b) summed;
+    in
+    sum (take 3 sorted);
 in
 {
   part1 = part1Answer ./input;
+  part2 = part2Answer ./input;
 }
