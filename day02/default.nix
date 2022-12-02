480d97624c52f8cde3ee6cc3fd3f3f506effef3e
Euan Kemp <euank@euank.com>
2022-12-01 21:34:07 -0800

    day02, part2

--- day02/default.nix
+++ day02/default.nix
@@ -23,8 +23,23 @@ let
       parsed = parseInput input;
     in
     foldl' (acc: el: acc + (winScore (head el) (elemAt el 1)) + (elemAt el 1)) 0 parsed;
+
+  # part 2
+  goalScore = l: r:
+    let
+      clamp = x: let m = mod x 3; in if m == 0 then 3 else m;
+    in
+    if r == 1 then clamp (l - 1) # lose, choose 1 less than theirs, i.e. they have rock (1), pick scissors (3), if they have paper (2) pick rock (1)
+    else if r == 2 then l + 3 # draw, 3 pts + hand score
+    else (clamp (l + 1)) + 6; # win
+
+  part2Answer = input:
+    let
+      parsed = parseInput input;
+    in
+    foldl' (acc: el: acc + (goalScore (head el) (elemAt el 1))) 0 parsed;
 in
 {
   part1 = part1Answer ./input;
-  #   part2 = part2Answer ./input;
+  part2 = part2Answer ./input;
 }
